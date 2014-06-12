21c52a0a73c1e4c7c34a62e98d09e6dfafd065f9
Brady Somerville <brady.somerville@gmail.com>
2014-06-12 13:52:29 -0500

    Add setting to config to get multiple results from UPS

--- lib/generators/spree_ups_address_validation/install/templates/config/initializers/spree_ups_address_validation.rb
+++ lib/generators/spree_ups_address_validation/install/templates/config/initializers/spree_ups_address_validation.rb
@@ -2,4 +2,5 @@ AddressValidator.configure do |config|
   config.key = 'insert_valid_key'
   config.username = 'insert_valid_username'
   config.password = 'insert_valid_password'
+  config.maximum_list_size = 5 # How many results do you want from UPS?
 end
