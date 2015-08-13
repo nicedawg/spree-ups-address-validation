f7d2ec05a9342609dfcc33d7946068f2d6822864
Brady Somerville <brady.somerville@gmail.com>
2015-08-13 15:33:51 -0500

    If 'Use Billing Address' checked, then also update billing address

--- app/assets/javascripts/spree/frontend/spree_ups_address_validation.js
+++ app/assets/javascripts/spree/frontend/spree_ups_address_validation.js
@@ -11,14 +11,25 @@ $(document).ready(function () {
 
     var $suggestion = $(this).parents(".ups-address-validation-suggestion");
 
-    $("#order_ship_address_attributes_address1").val( $(".ups-address-validation-suggestion-street1", $suggestion).html() );
-    $("#order_ship_address_attributes_address2").val( $(".ups-address-validation-suggestion-street2", $suggestion).html() );
-    $("#order_ship_address_attributes_city").val( $(".ups-address-validation-suggestion-city", $suggestion).html() );
+    var types = ['ship'];
+    if ($("#order_use_billing").is(':checked')) {
+      types.push('bill');
+    }
 
-    var state_id = $(".ups-address-validation-suggestion-state", $suggestion).data('state-id');
-    $("#order_ship_address_attributes_state_id option[value=" + state_id + "]").attr('selected', 'selected');
-    $("#order_ship_address_attributes_state_id").trigger('change');
+    $.each(types, function(index, value) {
+      $("#order_" + value + "_address_attributes_address1").val( $(".ups-address-validation-suggestion-street1", $suggestion).html() );
+      $("#order_" + value + "_address_attributes_address2").val( $(".ups-address-validation-suggestion-street2", $suggestion).html() );
+      $("#order_" + value + "_address_attributes_city").val( $(".ups-address-validation-suggestion-city", $suggestion).html() );
 
-    $("#order_ship_address_attributes_zipcode").val( $(".ups-address-validation-suggestion-zip", $suggestion).html() );
+      var state_id = $(".ups-address-validation-suggestion-state", $suggestion).data('state-id');
+      $("#order_" + value + "_address_attributes_state_id option[value=" + state_id + "]").attr('selected', 'selected');
+      $("#order_" + value + "_address_attributes_state_id").trigger('change');
+
+      $("#order_" + value + "_address_attributes_zipcode").val( $(".ups-address-validation-suggestion-zip", $suggestion).html() );
+    });
+
+    $('html, body').animate({
+      scrollTop: $("#billing").offset().top - 90
+    }, 1000);
   });
 });
